5f5983927c38a28b2e8e38d5cb0775085fa21b0d
X1 Carbon <yjl9903@vip.qq.com>
2018-12-14 13:10:47 +0800

    monaco-editor

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <xlorEditor></xlorEditor>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+// import MonacoEditor from 'vue-monaco'
+import xlorEditor from './components/xlor-editor'
 
 export default {
   name: 'app',
+  data(){
+    return {
+      
+    };
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    xlorEditor
+  },
+  mounted() {
+
   }
 }
 </script>
 
 <style>
-#app {
+* {
+  margin: 0;
+  padding: 0;
+}
+
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/xlor-editor.vue
@@ -0,0 +1,57 @@
+<template>
+<div id="xlor-editor">
+    <el-card shadow="always">
+        <monaco-editor
+            class="editor"
+            v-model="code"
+            :options="option"
+            language="python">
+        </monaco-editor>
+    </el-card>
+</div>
+</template>
+
+<script>
+import MonacoEditor from 'vue-monaco'
+
+export default {
+    data() {
+        return {
+            code: "print('Hello World')",
+            option: {
+                scrollBeyondLastLine: false,
+            }
+        };
+    },
+    components: {
+        MonacoEditor
+    },    
+};    
+</script>
+
+<style>
+#xlor-editor {
+    margin: 30px auto;
+    padding: 1px;
+    width: 80%;
+    height: 600px;
+    /* border: 1px solid black; */
+}
+#xlor-editor .el-card{
+    width: 100%;
+    height: 100%;
+}
+#xlor-editor .el-card>div {
+    margin: 0 auto;
+    width: 96%;
+    height: 96%;
+}
+#xlor-editor .editor {
+  /* margin: 20px auto; */
+  width: 100%;
+  height: 100%;
+  /* height: 600px; */
+  /* border: 1px solid black; */
+  /* padding: 1px; */
+}
+</style>
